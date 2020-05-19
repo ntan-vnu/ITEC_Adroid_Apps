fe8a1aae4ef7e522951dc844dc16f5314f52aa4f
ntan <nguyenthanhan1001@gmail.com>
2020-05-19 15:28:49 +0700

    week 02

--- DemoListview/app/src/main/java/hcmus/selab/ntan/demolistview/MainActivity.java
+++ DemoListview/app/src/main/java/hcmus/selab/ntan/demolistview/MainActivity.java
@@ -20,7 +20,16 @@ public class MainActivity extends AppCompatActivity {
 
     private void generateData() {
         _listHeroes = new ArrayList<>();
-        
+        _listHeroes.add(new Hero("Ahri", R.drawable.ahri, "This is a dummy description..."));
+        _listHeroes.add(new Hero("Ashe", R.drawable.ashe, "This is a dummy description..."));
+        _listHeroes.add(new Hero("Brand", R.drawable.brand, "This is a dummy description..."));
+        _listHeroes.add(new Hero("Elise", R.drawable.elise, "This is a dummy description..."));
+        _listHeroes.add(new Hero("Hecarim", R.drawable.hecarim, "This is a dummy description..."));
+        _listHeroes.add(new Hero("Janna", R.drawable.janna, "This is a dummy description..."));
+        _listHeroes.add(new Hero("Leblanc", R.drawable.leblanc, "This is a dummy description..."));
+        _listHeroes.add(new Hero("Masteryi", R.drawable.masteryi, "This is a dummy description..."));
+        _listHeroes.add(new Hero("Nasus", R.drawable.nasus, "This is a dummy description..."));
+        _listHeroes.add(new Hero("Tryndamere", R.drawable.tryndamere, "This is a dummy description..."));
 
     }
 }
